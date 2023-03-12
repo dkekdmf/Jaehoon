153c402fdf2ebb0ae6939230bbf132df1aca6e48
jaehoon <dkekdmf@naver.com>
2023-03-13 00:00:15 +0900

    2023-03-13 code up 100

---
+++ CodeUp/1028.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main_1028(void) {
+	unsigned int n;
+	scanf("%u", &n);
+	printf("%u", n);
+
+
+
+}
---
+++ CodeUp/1029.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main_1029(void) {
+	double n;
+	scanf("%lf", &n);
+	printf("%.11lf", n);
+
+
+	return 0;
+}
