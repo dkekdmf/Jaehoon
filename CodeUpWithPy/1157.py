7eea25f1161edf6c8d06c75ecb1c61dd4604c341
jaehoon <dkekdmf@naver.com>
2023-04-18 23:34:12 +0900

    2023-04-18 python code up

---
+++ CodeUpWithPy/1157.py
@@ -0,0 +1,6 @@
+a = float(input())
+
+if a>=50 and a<=60:
+    print('win')
+else:
+    print('lose')
---
+++ CodeUpWithPy/1159.py
@@ -0,0 +1,6 @@
+a = int(input())
+
+if (a>=50 and a<=70) or a%6==0:
+    print('win')
+else:
+    print('lose')
