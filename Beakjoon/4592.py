3d63173b912c683efaf8f70663b533085870e5fe
jaehoon <dkekdmf@naver.com>
2023-05-28 01:41:31 +0900

    2023-05-28 백준 python

---
+++ Beakjoon/4592.py
@@ -0,0 +1,18 @@
+
+while True:
+    ans = []
+    a= input().split()
+    print(type(ans))
+    if a[0]=='0':
+        break
+    a.remove(a[0])
+    for i in a:
+        if len(ans) ==0:
+            ans.append(i)
+        elif ans[-1]!=i:
+            ans.append(i)
+    ans.append('$')
+    # print(' '.join(ans))
+       
+
+    
