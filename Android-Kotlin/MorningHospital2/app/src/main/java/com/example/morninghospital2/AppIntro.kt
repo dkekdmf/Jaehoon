c2bd56893b157932d5028df26150db933b4e40b7
jaehoon <dkekdmf@naver.com>
2023-06-06 00:40:32 +0900

    2023-06-06 morning hospital 앱 설명

---
+++ Android-Kotlin/MorningHospital2/app/src/main/java/com/example/morninghospital2/AppIntro.kt
@@ -0,0 +1,26 @@
+package com.example.morninghospital2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class AppIntro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_intro)
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        },1400)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
